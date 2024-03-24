2e60879daedb80ad4c77e1f7d5587e55273cc43e
sumit <skipper.sumit@gmail.com>
2024-03-24 13:21:56 +0530

    DFS Bipartite Graph

--- Graphs/Bipartite Graph DFS/code.cpp
+++ Graphs/Bipartite Graph DFS/code.cpp
@@ -1,29 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool check(int start, int V, vector<int>adj[], int color[]) {
-    queue<int> q;
-    q.push(start);
-    color[start] = 0;
-    while (!q.empty()) {
-        int node = q.front();
-        q.pop();
+bool dfs(int node, int col, int color[], vector<int> adj[]) {
+    color[node] = col;
 
-        for (auto it : adj[node]) {
-            // if the adjacent node is yet not colored
-            // you will give the opposite color of the node
-            if (color[it] == -1) {
-
-                color[it] = !color[node];
-                q.push(it);
-            }
-            // is the adjacent guy having the same color
-            // someone did color it on some other path
-            else if (color[it] == color[node]) {
-                return false;
-            }
+    // traverse adjacent nodes
+    for (auto it : adj[node]) {
+        // if uncoloured
+        if (color[it] == -1) {
+            if (dfs(it, !col, color, adj) == false) return false;
+        }
+        // if previously coloured and have the same colour
+        else if (color[it] == col) {
+            return false;
         }
     }
+
     return true;
 }
 
@@ -31,12 +23,11 @@ bool isBipartite(int V, vector<int>adj[]) {
     int color[V];
     for (int i = 0; i < V; i++) color[i] = -1;
 
+    // for connected components
     for (int i = 0; i < V; i++) {
-        // if not coloured
         if (color[i] == -1) {
-            if (check(i, V, adj, color) == false) {
+            if (dfs(i, 0, color, adj) == false)
                 return false;
-            }
         }
     }
     return true;
@@ -58,7 +49,6 @@ int main()
     addEdge(adj, 3, 1);
 
     // any graph with odd length cycle can never be a bipartite graph
-
     if (isBipartite(4, adj))
         cout << "Graph is Bipartite Graph\n";
     else
